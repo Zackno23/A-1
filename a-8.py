cf9e6354d75ba999f0a2f0121aa120255ba87a91
chikara <chikara.f.yoshida@gmail.com>
2018-11-20 12:26:31 +0900

    リストの中のリスト、出てこいや っていうときに、 print(list[1,0]) とすると、 typeError: list indices must be integers or slices, not tuple とエラーが出る。 list[1][0] とすれば出力される numpy.arrayをインポートすればできるらしいが、今はやめておこう。 https://qiita.com/toda_tech/items/f9caa8621ca0c1fa3f98

---
+++ a-8.py
@@ -0,0 +1,5 @@
+users_info = [["Kazuma", 35],
+              ["Tom", 57],
+              ["Bob", 77]]
+for i in range(len(users_info)):
+    print(f"Name: {users_info[i][0]}, Age: {users_info[i][1]}")
